936efcdd5eee155fce6d1ada6f6cbc17bef1baa5
eldin001 <eldin.balje@icloud.com>
2024-03-11 14:29:43 +0100

    start dice game project

--- script.js
+++ script.js
@@ -2,20 +2,8 @@
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-3. si ma noi cosa vogliamo fare?
-4. torniamo a scrivere in italiano
-5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
-P.S Dadi */
+
+/* email */
 
 let btnElement = document.querySelector('.btn');
 
@@ -39,6 +27,41 @@ btnElement.addEventListener('click', function () {
         result.innerHTML = 'Utenete non riconocsciuto';
     }
 })
+
+/* gioco dei dadi */
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
+P.S Dadi */ 
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let userScore = '';
+let computerScore = '';
+
+let diceButtonElement = document.getElementById('outcome');
+
+diceButtonElement.addEventListener('click', function(){
+    let userScore = document.getElementById('userNumber').value;
+    console.log(userScore)
+})
+
+
+
+
 
 
 
